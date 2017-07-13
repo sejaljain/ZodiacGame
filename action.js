30693f02f405a88a55f6198d93f3500ba247e5ac
tamyawilliams <tamyawilliams2@gmail.com>
2017-07-13 11:18:38 -0400

    some changes

--- action.js
+++ action.js
@@ -1,26 +1,39 @@
-var total = 0
+var point_levels = new Array();
+point_levels["A"]=5;
+point_levels["B"]=3
+point_levels["C"]=1
+point_levels["D"]=0
 
-function score(id){
-    if (id == "A"){
-      total = total+5
+function getTotalPoints()
+{
+  var TotalPoints=0
+  var theForm = document.forms["cakeform"]
+  var listofbuttons = theForm.elements["selectedcake"]
+
+  for(i = 0; i < listofbuttons.length; i++)
+  {
+    if(listofbuttons[i].checked);
+    {
+      TotalPoints += point_levels[listofbuttons[i].value];
     }
-    else if (id == "B"){
-      total = total+3
-    }
-    else if (id == "C"){
-      total = total+1
-    }
+  }
+  return TotalPoints;
 }
 
-if (total > 50){
-  document.getElementById('yoursign').innerHTML="Gemini";
-}
-else if (total > 40){
-  document.getElementById('yoursign').innerHTML="Capricorn";
-}
-else if (total > 30){
-  document.getElementById('yoursign').innerHTML="Leo"
-}
-else (total > 20){
-  document.getElementById('yoursign').innerHTML="Taurus"
+function getSign()
+{
+    var total = getTotalPoints();
+
+    if (total > 50){
+      document.getElementById('yoursign').innerHTML="Gemini";
+    }
+    else if (total > 40){
+      document.getElementById('yoursign').innerHTML="Capricorn";
+    }
+    else if (total > 30){
+      document.getElementById('yoursign').innerHTML="Leo"
+    }
+    else (total > 20){
+      document.getElementById('yoursign').innerHTML="Taurus"
+    }
 }
